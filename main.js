24c04cd97c43116e957c770b4dced02f78ff2db0
foxthefox <16841643+foxthefox@users.noreply.github.com>
2024-01-30 17:17:04 +0100

    HA28

--- main.js
+++ main.js
@@ -987,7 +987,7 @@ class EcoflowMqtt extends utils.Adapter {
 			if (recon_timer) clearTimeout(recon_timer);
 			if (lastQuotInterval) clearInterval(lastQuotInterval);
 			if (this.haClient && this.haDevices) {
-				await ha.publishAsync(this, this.config.haTopic + '/iob/info/status', 'offline', 1);
+				ha.publishAsync(this, this.config.haTopic + '/iob/info/status', 'offline', 1);
 				/*
 				this.haClient.publish(this.config.haTopic + '/iob/info/status', 'offline', { qos: 1 }, (error) => {
 					if (error) {
@@ -998,12 +998,7 @@ class EcoflowMqtt extends utils.Adapter {
 				});
 				*/
 				for (let i = 0; i < this.haDevices.length; i++) {
-					await ha.publishAsync(
-						this,
-						this.config.haTopic + '/' + this.haDevices[i] + '/info/status',
-						'offline',
-						1
-					);
+					ha.publishAsync(this, this.config.haTopic + '/' + this.haDevices[i] + '/info/status', 'offline', 1);
 					/*
 					this.haClient.publish(
 						this.config.haTopic + '/' + this.haDevices[i] + '/info/status',
