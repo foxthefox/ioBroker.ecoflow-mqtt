26d7220d951e7b8d9943c8b7774b41cadefd56cf
foxthefox <16841643+foxthefox@users.noreply.github.com>
2024-08-10 17:07:24 +0200

    0.0.39

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,9 +1,20 @@
-import globals from "globals";
-import pluginJs from "@eslint/js";
-
+import globals from 'globals';
+import pluginJs from '@eslint/js';
 
 export default [
-  {files: ["**/*.js"], languageOptions: {sourceType: "commonjs"}},
-  {languageOptions: { globals: globals.browser }},
-  pluginJs.configs.recommended,
+	{ files: [ '**/*.js' ], languageOptions: { sourceType: 'commonjs' } },
+	{
+		ignores: [
+			'.dev-server/**/*',
+			'admin/build/**/*',
+			'admin/words.js',
+			'test/**/*',
+			'main.test.js',
+			'lib/**/*',
+			'!lib/ecoflow_utils.js',
+			'!lib/adapter_utils.js'
+		]
+	},
+	{ languageOptions: { globals: globals.browser } },
+	pluginJs.configs.recommended
 ];
